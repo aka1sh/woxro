56b8c92bdd5f4dc7ed9db1c49c1298930bf8cf7e
aka1sh <107101265+aka1sh@users.noreply.github.com>
2023-12-27 19:34:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div class="close-button">
+    <span class="cross">&#10006;</span>
+  </div>
+      <header className="App-header">
+      </header>
+      <img id="p1" src="p1.png"></img>
+      <img src="fortnite-pic-1.png" alt="img"></img>
+      <img id="p2" src="p2.png"></img>
+      <div class="button">
+         <img id="b1" src="b1.png"></img>
+      </div>
+
+      <div class="image-gallery">
+    <div class="image-container">
+      <img src="1.png" alt=""></img>
+      <p class="image-description">Explore large, destructible environments where no two games are ever the same.</p>
+    </div>
+    <div class="image-container">
+      <img src="2.png" alt="Image 2"></img>
+      <p class="image-description">Team up with friends by sprinting, climbing and smashing your way to earn your Victory Royale</p>
+    </div>
+    <div class="image-container">
+      <img src="3.png" alt="Image 3"></img>
+      <p class="image-description">Discover even more ways to play across thousands of creator-made game genres</p>
+    </div>
+  </div>
+
+
+
+      <img id="g1" src="g1.png"></img>
+      <img id="g2" src="g2.png"></img>
+      <img id="g3" src="g3.png"></img>
+
+     
+      {/* <img id="m1" src="m1.png"></img>
+      <img id="m2" src="m2.png"></img>
+      <img id="m3" src="m3.png"></img> */}
+      
+      {/* <div  class="image-row">
+      <img id="m1" src="m1.png" alt="m1"></img>
+      <img id="m2" src="m2.png" alt="m2"></img>
+      <img id="m3" src="m3.png" alt="m3"></img>
+      </div> */}
+    </div>
+    
+  );
+}
+
+export default App;
